0b7510601f29b2445afa73c51d755e6cbc79a33c
memojtabazamani <memojtabazamani@gmail.com>
2021-07-22 23:57:56 +0430

    done: ارتباط با فایل api موفقیت آمیزه و پیام نشان داده میشه

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 //  Initial
+const request 	    = new XMLHttpRequest();
+const url     	    = "api.php";
+const method        = "GET";
+const sectionWork   = document.getElementById('sectionWork');
+const sectionWorkHelp = document.getElementById('sectionWorkHelp');
+let selectedSection = 0;
+
+sectionWork.onchange = function(e) {
+	if((selectedSection = sectionWork.selectedIndex) === 0) {
+        sectionWork.setCustomValidity("Must Be Selected Section Work!");
+        sectionWorkHelp.innerHTML = sectionWork.validationMessage;	
+	} else {
+		sectionWorkHelp.innerHTML = "";
+		request.open(method, url);
+		request.onreadystatechange = handleAjax;
+		request.send();
+	}
+	
+}
+
+function handleAjax() {
+	if(request.status === 200 && request.readyState === 4) {
+		console.log(request);
+	}
+}
+
